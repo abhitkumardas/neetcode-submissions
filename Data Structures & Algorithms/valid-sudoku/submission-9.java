078e88c0c03149dbe7724f52f113b11f949fa022
Abhit Kumar Das <avitdas00@gmail.com>
2026-05-02 10:12:24 +0530

    Add: valid-sudoku - submission-9

---
+++ Data Structures & Algorithms/valid-sudoku/submission-9.java
@@ -0,0 +1,28 @@
+class Solution {
+    public boolean isValidSudoku(char[][] board) {
+        boolean[][] row = new boolean[9][9];
+        boolean[][] column = new boolean[9][9];
+        boolean[][] box = new boolean[9][9];
+
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                if (board[i][j] == '.')
+                    continue;
+
+                int num = board[i][j] - '1'; // index structure 0-8 and nums 1-9
+
+                int boxIndex = (i / 3) * 3 + (j / 3);
+
+                if (row[i][num] || column[j][num] || box[boxIndex][num]) {
+                    return false;
+                }
+
+                row[i][num] = true;
+                column[j][num] = true;
+                box[boxIndex][num] = true;
+            }
+        }
+
+        return true;
+    }
+}
