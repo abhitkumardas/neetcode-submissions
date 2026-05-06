011811322e5b41380d4deb9726b3f62c55537053
Abhit Kumar Das <avitdas00@gmail.com>
2026-05-06 23:03:49 +0530

    Add: valid-sudoku - submission-13

---
+++ Data Structures & Algorithms/valid-sudoku/submission-13.java
@@ -0,0 +1,26 @@
+class Solution {
+    public boolean isValidSudoku(char[][] board) {
+        Set<String> seen = new HashSet();
+
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                if (board[i][j] == '.') {
+                    continue;
+                }
+
+                char num = board[i][j];
+                int boxIndex = (i / 3) * 3 + (j / 3);
+
+                String rowString = num + "_" + i + "row";
+                String columnString = num + "_" + j + "col";
+                String boxString = num + "_" + boxIndex + "box";
+
+                if (!seen.add(rowString) || !seen.add(columnString) || !seen.add(boxString)) {
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
+}
